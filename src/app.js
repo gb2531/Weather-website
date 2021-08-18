ae14cfd34bb1a121d39d94d525c64caa48e42e1c
surya gb <surya.gb@turntechindia.co.in>
2021-08-18 16:17:38 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-
+const port = process.env.PORT || 80
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -96,6 +96,6 @@ app.get('*', (req,res) => {
     })
 })
 
-app.listen(80,() => {
-    console.log('server is running on port 80')
+app.listen(port,() => {
+    console.log('server is running on port '+port)
 })
